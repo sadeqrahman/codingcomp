325d6fe8574da3db0ccf2e768c5e109e6cb54d70
sadeqrahman <44654067+sadeqrahman@users.noreply.github.com>
2018-10-31 23:32:46 +0000

    q4 solution

--- src/main/java/answers/Question4.java
+++ src/main/java/answers/Question4.java
@@ -3,8 +3,56 @@ package answers;
 public class Question4 {
 
 	public static int selectionFailedTradedesks(String[][] rows, int numberMachines) {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		int sys = rows.length;
+		int pcs = rows[0].length;
+		
+		if(sys < 0 || sys > 100) return 0;
+		if(pcs < 0 || pcs > 100) return 0;
+		if(pcs < numberMachines) return 0;
+		
+		numberMachines--;
+
+		
+		int min = Integer.MAX_VALUE;
+		int start = 0;
+		int end = 0;
+		int sum = 0;
+		
+		for(int i=0; i < sys; i++) {
+			
+			start = 0;
+			end = 0;
+			sum = 0;
+			
+			for(int j=0; j<pcs; j++) {
+				
+				String pc = rows[i][j];
+				
+				if(pc.length() == 1 && pc.equals("X")) {
+					sum = 0;
+					start = i + 1;
+					end = i + 1;
+				}else {
+					sum += Integer.parseInt(rows[i][end]);
+					
+					if((end - start) == numberMachines){
+						
+						if(sum < min) {
+							min = sum;
+							sum -= Integer.parseInt(rows[i][start]);
+							start++;
+						}
+					
+					}
+					end++;
+				}
+				
+			}
+			
+		}
+		
+		return min;
 	}
 
 }
