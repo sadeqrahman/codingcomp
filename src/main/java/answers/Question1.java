28003155f41897811331ea63ebdf347793fce3ad
sadeqrahman <44654067+sadeqrahman@users.noreply.github.com>
2018-10-31 23:30:07 +0000

    q1 solution

--- src/main/java/answers/Question1.java
+++ src/main/java/answers/Question1.java
@@ -3,8 +3,19 @@ package answers;
 public class Question1 {
 
 	public static int bestMergedPortfolio(int[] portfolios) {
-		// TODO Auto-generated method stub
-		return 0;
+		int n = portfolios.length;
+		int max = 0;
+		for (int i = 0; i < n-1; i++) {
+			int xl = portfolios[i];
+			for (int j = i; j < n; j++) {
+				int value = xl ^ portfolios[j];
+				if (value > max) {
+					max = value;
+					if(max == 65535) return 65535;
+				}
+			}
+		}
+		return max;
 	}
 
 }
